de4916ed7f44e326fafccb387263d383817353b5
Baralexon <ban67@yandex.ru>
2022-08-24 12:06:54 +0700

    Добавьте файлы проекта.

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7
+{
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
